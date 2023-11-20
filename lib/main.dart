6c9f4b45669b790067b5288b97007bc236d1bbe0
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2023-11-20 21:06:20 +0330

    finish up the design of login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_first_app/login_page.dart';
+import 'package:flutter_first_app/styles/app_colors.dart';
  
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "My First Application",
-      theme: ThemeData(fontFamily: 'Urbanist'),
+      theme: ThemeData(
+                        fontFamily: 'Urbanist', 
+                        scaffoldBackgroundColor: AppColors.background,
+                      ),
       home: const LoginPage(),
     );
   }

cc357a4215bd0f9b396fd62dfb5af1422fbe3d2a
Bahar Keshavarz <bahar.keshavarzc@gmail.com>
2023-11-27 12:17:47 +0330

    learn futurebuilder in flutter

--- lib/config/app_routes.dart
+++ lib/config/app_routes.dart
@@ -1,17 +1,20 @@
 import 'package:flutter_first_app/pages/edit_profile_page.dart';
 import 'package:flutter_first_app/pages/home_page.dart';
+import 'package:flutter_first_app/pages/list_page.dart';
+import 'package:flutter_first_app/pages/load_data_page.dart';
 import 'package:flutter_first_app/pages/main_page.dart';
 import 'package:flutter_first_app/pages/nearby_page.dart';
 import 'package:flutter_first_app/pages/test_page.dart';
 
 class AppRoutes {
  static final pages = {
-        "/": (context) => const MainPage(),
+        "/": (context) => const FutureBuilderExampleApp(),
         "/home": (context) => HomePage(),
         "/main": (context) => const MainPage(),
         "/test": (context) => const TestPage(),
         "/edit_profile": (context) => const EditProfilePage(),
         "/nearby": (context) => const NearbyPage(),
+        "/list": (context) => const MyListView(),
       };
 
   static const login = '/';
@@ -20,4 +23,6 @@ class AppRoutes {
   static const editProfile = '/edit_profile';
   static const nearby = '/nearby';
   static const user = '/user';
+  static const lists = '/list';
+  static const fetchData = "/fetch";
 }
